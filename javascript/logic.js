71b27ee46fe55f44a20a3e349d136979c77b9642
Bernard Kwok <kwokcb@gmail.com>
2022-04-06 10:48:03 -0400

    Revert "Extract out data to be filled in dynamically."
    This reverts commit 7a658c147466efd306d94de58f8e0c3dc674b860.

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,57 +1,28 @@
 
-/*
-    Switch between ingredients based on ingredient item
-*/
+
 function switchIngredient(item) 
 {
     document.getElementById("ingredientsMenu").innerHTML = item;
-    const pickedIngredient = ingredients.get(item);
+    pickedIngredient = ingredients.get(item);
 
-    const mainImage = document.getElementById("main-image");
+    mainImage = document.getElementById("main-image");
     mainImage.src = pickedIngredient.imageURL;
 
-    const benefits = document.getElementById("benefits-text");
-    benefits.innerHTML = pickedIngredient.benefitsText;
+   benefits = document.getElementById("benefits-text");
+   benefits.innerHTML = pickedIngredient.benefitsText;
 
-    const origin = document.getElementById("history-text");
-    origin.innerHTML = pickedIngredient.historyOriginText;
+   origin = document.getElementById("history-text");
+   origin.innerHTML = pickedIngredient.historyOriginText;
 
-    const map = document.getElementById("origin-map");
-    map.src = pickedIngredient.mapURL;
+   map = document.getElementById("origin-map");
+   map.src = pickedIngredient.mapURL;
 
-    const title = document.getElementById("ingredientName");
-    title.innerHTML = pickedIngredient.uinameText;
+   title = document.getElementById("ingredientName");
+   title.innerHTML = pickedIngredient.nameText;
 
-    const hotCold = document.getElementById("hotCold");
-    hotCold.src = pickedIngredient.hotColdURL;
-
-    const category = document.getElementById("category");
-    category.src = pickedIngredient.categoryURL;
-}
-
-/*
-    Add menu items based on ingredients list
- */ 
-function addMenuItems()
-{
-    const ingredientMenu = document.getElementById("ingredientMenu");
-    if (ingredientMenu) {
-        var ingredientMenuText = "";
-        ingredients.forEach(ing => {
-            ingredientMenuText +=
-                "<li><a class='dropdown-item' href='#' onclick=\"switchIngredient('" +
-                ing.uiname + "')\">" + ing.uiname + "</a></li>";
-        });
-        ingredientMenu.innerHTML = ingredientMenuText;
-    }
-}
-
-/*
-    Initialize template
-*/
-function initializeTemplate()
-{
-    loadData();
-    addMenuItems();
-    switchIngredient('Mushroom');
+   hotCold = document.getElementById("hotCold");
+   hotCold.src = pickedIngredient.hotColdURL;
+    
+   category = document.getElementById("category");
+   category.src = pickedIngredient.categoryURL;
 }
